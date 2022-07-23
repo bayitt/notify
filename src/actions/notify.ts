8f744116180bfbf3fd3e349b5ae0b5e5a1b7b1ef
olamileke <olamileke.f@gmail.com>
2022-07-23 05:04:02 +0100

    feat: added valid step validation + tested verify middleware

--- src/actions/notify.ts
+++ src/actions/notify.ts
@@ -1,10 +1,52 @@
 import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
+import { Mail } from "../channels";
 
-export const notify = (req: Request, res: Response, next: NextFunction) => {
+type TStatusTextFunc = (service: string) => string;
+
+export const statuses: { [key: string]: (string | TStatusTextFunc)[] } = {
+  build_start: [
+    "Build Started",
+    (service: string) => `The ${service} build has started.`,
+  ],
+  build_completed: [
+    "Build Completed",
+    (service: string) => `The ${service} build is done.`,
+  ],
+  build_pushed: [
+    "Image Pushed",
+    (service: string) =>
+      `The just built image for ${service} has been successfully pushed to Dockerhub.`,
+  ],
+  container_recreated: [
+    "Container Recreated",
+    (service: string) =>
+      `The container for ${service} has been successfully recreated based on the latest image.`,
+  ],
+  pipeline_failed: [
+    "Pipeline Failed",
+    (service: string) => `The build pipeline for ${service} has failed.`,
+  ],
+};
+
+export const notify = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const errors = validationResult(req);
 
-  if (!errors.isEmpty()) res.status(400).json({ errors: errors.array() });
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() });
 
   const { service, step } = req.body;
+
+  const params = statuses[step.toLowerCase()];
+  const subject = `${service} - ${params[0]}`;
+  const text = (params[1] as TStatusTextFunc)(service);
+
+  const mail = new Mail();
+  await mail.send(subject, text);
+
+  res.status(200).json({ message: "Notification sent successfully" });
 };
--- src/routes/notify.ts
+++ src/routes/notify.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { body } from "express-validator";
-import { notify } from "../actions";
+import { notify, statuses } from "../actions";
 import { verifyRequestMiddleware } from "../middleware";
 
 export const notifyRouter = Router();
@@ -8,7 +8,13 @@ export const notifyRouter = Router();
 notifyRouter.post(
   "/",
   body("service", "service is required").exists(),
-  body("step", "step is required").exists(),
+  body("step", "step is required")
+    .exists()
+    .custom((step: string) => {
+      const steps = Object.keys(statuses);
+      if (steps.includes(step.toLowerCase())) return true;
+      throw new Error(`${step} is not a valid step`);
+    }),
   verifyRequestMiddleware,
   notify
 );
